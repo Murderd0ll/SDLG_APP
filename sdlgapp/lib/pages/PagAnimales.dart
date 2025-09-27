3cc92283851c1ed13e989ee4fa5193a1388492e1
murderd0ll <moran.reyna.6mt@gmail.com>
2025-09-26 23:08:44 -0700

    **Una tabla hjasdjh no nada más pq no he hecho la bdd..

--- sdlgapp/lib/pages/PagAnimales.dart
+++ sdlgapp/lib/pages/PagAnimales.dart
@@ -6,9 +6,35 @@ class PagAnimales extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Animales", style: TextStyle(fontSize: 30)),
-    ); //Este solo es el texto pero despuésse debe agregar una tabla que muestre los datos de los animales
-    // un cuadro de busqueda y un botón para agregar nuevos
+    return Scaffold(
+      body: Table(
+        border: TableBorder.symmetric(
+          inside: BorderSide(
+            width: 1,
+            color: const Color.fromARGB(255, 56, 56, 56),
+          ),
+        ),
+        children: [
+          TableRow(
+            children: [Text(' Sexo'), Text(' Arete'), Text(' F. Nacimiento')],
+          ),
+          TableRow(
+            children: [
+              Text('Row 1, Col 1'),
+              Text('Row 1, Col 2'),
+              Text('Row 1, Col 3'),
+            ],
+          ),
+          TableRow(
+            children: [
+              Text('Row 2, Col 1'),
+              Text('Row 2, Col 2'),
+              Text('Row 2, Col 3'),
+            ],
+          ),
+        ],
+      ),
+    );
+    // falta un cuadro de busqueda y un botón para agregar nuevos
   }
 }
